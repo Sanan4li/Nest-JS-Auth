66f722427c21ebc160af1ec62c7e062c8b70d380
Sanan4li <Alisanan620@gmail.com>
2023-01-26 01:24:54 +0500

    added authentication and authorization

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '../users/user.model';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.auth';
+import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '60s' },
+    }),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+  ],
+  providers: [AuthService, UsersService, LocalStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.model';
+import { UsersController } from './users.controller';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+export class UsersModule {}
